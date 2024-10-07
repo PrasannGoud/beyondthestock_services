aa98ff4b0248dd0fd7aa7087200f3f6ebc5f1eae
Suraj Seth <SETH.SURAJ752@GMAIL.COM>
2024-10-08 01:08:15 +0530

    base code

---
+++ src/main/java/com/beyondthestock/beyondthestock_service/BeyondthestockServiceApplication.java
@@ -0,0 +1,13 @@
+package com.beyondthestock.beyondthestock_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeyondthestockServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeyondthestockServiceApplication.class, args);
+	}
+
+}
